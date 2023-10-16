ef64e3e3072fbe6b3c7330abdb287099ec6fc1e6
Eze Israel John <ezeisrael499@gmail.com>
2023-10-16 23:36:33 +0100

    converts an integer to a specified base and stores the result as a string in the buffer

---
+++ _itob.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#define BIN 2
+#define OCT 8
+#define DEC 10
+#define HEX 16
+
+/**
+ * _itob - convert integer to the specified base and stores the result
+ * as a string
+ *
+ * @number: the number to convert
+ * @buffer: the string buffer to store the result
+ * @base: the base to convert @number to
+ */
+void _itob(ssize_t number, char *buffer, int base)
+{
+	int i = 0;
+	long long sign = number;
+
+	if (number < 0)
+		number = -number;/*If number is negative, take its absolute value */
+
+	switch (number)
+	{
+		/*bases accepted*/
+		case BIN
+		case OCT
+		case DEC
+		case HEX
+			/*Convert remainder to the appropriate character representation*/
+			do {
+				buffer[i++] = (number % base > 9) ? 'a' + (number % base - 10) :
+					(number % base + '0');
+			}
+			while ((number /= base) > 0);
+
+			if (sign < 0)
+			{
+				buffer[i++] = '-';
+				/*Reverse the string to get the correct representation*/
+				_reverse(buffer, i);
+				buffer[i] = '\0';
+				break;
+				default:
+				return;
+			}
+	}
+}
