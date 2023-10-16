c3e0d770746288fea3cfb6c9551ed6bd74c63db5
Eze Israel John <ezeisrael499@gmail.com>
2023-10-16 23:30:46 +0100

    Reverses a given string in place

---
+++ _reverse_str.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _reverse_str - reverse string in place
+ *
+ * @buffer: string to reverse
+ * @len: string length
+ */
+void _reverse_str(char *buffer, int len)
+{
+	/* swap positions */
+	for (int i = 0; j = len - 1; i < j; i++, j--)
+	{
+		char temp = buffer[i];
+
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
