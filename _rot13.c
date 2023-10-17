ecb2d60166e9429c4a82b60f5f637e76782d120a
Eze Israel John <ezeisrael499@gmail.com>
2023-10-17 02:43:58 +0100

    takes a pointer to a string and modifies the string in-place

---
+++ _rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ * Return: pointer to encoded string
+ */
+char *rot13(const char *s)
+{
+	int i, j;
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
