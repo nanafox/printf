bb1245e45e684068e54f4a774c0abf745c36e6f9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-17 00:18:29 +0000

    Added functions to handle the %x and %X format specifiers

---
+++ handle_hexadecimals.c
@@ -0,0 +1,56 @@
+
+/**
+ * handle_hex_lower - convert and append an integer in lower hexadecimal
+ * format
+ * @spec: format specifier information (unused)
+ * @args: arguments list
+ * @buffer: string buffer to store the result
+ *
+ * Return: the number of characters appended to the string buffer
+ */
+int handle_hex_lower(__attribute__((unused)) const format_specifier * spec,
+		va_list args, string_buffer *buffer)
+{
+	char hex_str[17];
+	ssize_t n = va_arg(args, ssize_t);
+	size_t initial_length = buffer->length;
+	int characters_added;
+
+	_itob(n, hex_str, HEX);
+	append_string(buffer, hex_str);
+
+	characters_added = buffer->length - initial_length;
+	return (characters_added);
+}
+
+/**
+ * handle_hex_upper - convert and append an integer in uppercase hexadecimal
+ * format
+ * @spec: format specifier information (unused)
+ * @args: arguments list
+ * @buffer: string buffer to store the result
+ *
+ * Return: the number of characters appended to the string buffer
+ */
+int handle_hex_upper(__attribute__((unused)) const format_specifier * spec,
+		va_list args, string_buffer *buffer)
+{
+	int i;
+	char hex_str[17];
+	ssize_t n = va_arg(args, ssize_t);
+	size_t initial_length = buffer->length;
+	int characters_added;
+
+	_itob(n, hex_str, HEX);
+	for (i = 0; hex_str[i] != '\0'; i++)
+	{
+		if (hex_str[i] >= 'a' && hex_str[i] <= 'f')
+		{
+			hex_str[i] = hex_str[i] - 'a' + 'A';
+		}
+	}
+	append_string(buffer, hex_str);
+
+	characters_added = buffer->length - initial_length;
+	return (characters_added);
+}
