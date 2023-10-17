3680afeab5bdf89b1523482be7ed2c81d6763f9d
Eze Israel John <ezeisrael499@gmail.com>
2023-10-17 01:21:27 +0100

    Calculate the length of a string

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Counts the length of a string
+ * @s: the operand used
+ *
+ * Return: The length of the string
+ */
+size_t _strlen(const char *s)
+{
+	int count;
+	
+	count = 0;
+
+	for (; s; count++)
+		s++; /* move to the next character */
+	return (count);
+}
