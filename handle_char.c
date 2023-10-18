bc79496477c0af7784f1a610a62256bdeb2c592e
Maxwell Nana Forson <48143641+nanafox@users.noreply.github.com>
2023-10-18 01:17:16 +0000

    updated handle char

--- handle_char.c
+++ handle_char.c
@@ -11,11 +11,15 @@
 int handle_char(__attribute__((unused))
 		const format_specifier * spec, va_list args, string_buffer *buffer)
 {
+	int characters_added;
+	size_t initial_length;
 	char ch = va_arg(args, int);
 
+	initial_length = buffer->length;
 	append_char(buffer, ch);
 
-	return (1);
+	characters_added = buffer->length - initial_length;
+	return (characters_added);
 }
 
 /**
