94c8d397a2190a39e78e4f8f13711361fe222596
Maxwell Nana Forson <48143641+nanafox@users.noreply.github.com>
2023-10-18 00:19:41 +0000

    code update

--- parse_modifiers.c
+++ parse_modifiers.c
@@ -8,38 +8,42 @@
  *
  * Return: a pointer to the updated format
  */
-const char *parse_modifiers(const char *format, format_specifier *spec, string_buffer *buffer)
+const char *parse_modifiers(const char *format, format_specifier *spec,
+							string_buffer *buffer)
 {
-    const char *fmt_marks = "idcsSrRuxXop";
+	const char *fmt_marks = "%idcsSrRuxXop";
 
-    while (*format && !_strchr(fmt_marks, *format)) 
+	spec->sharp_flag = 0;
+
+	while (*format && !_strchr(fmt_marks, *format))
 	{
-        switch (*format) 
+		switch (*format)
 		{
-            case '1': case '2': case '3': case '4':
-            case '5': case '6': case '7': case '8': case '9':
-                handle_width(format, spec, buffer);
-                break;
-            case '.':
-                handle_precision(format, spec, buffer);
-                break;
-            case 'l':
-                spec->length = 'l';
-                break;
-            case 'h':
-                spec->length = 'h';
-                break;
-            case '#':
-                spec->sharp_flag = 1;
-                break;
-            case '+':
-                spec->plus_flag = 1;
-                break;
-            case ' ':
-                spec->space_flag = 1;
-                break;
-        }
-        format++;
-    }
-    return (format);
+		case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+			handle_width(format, spec, buffer);
+			break;
+		case '.':
+			handle_precision(format, spec, buffer);
+			break;
+
+		case 'l':
+			spec->length = 'l';
+			break;
+		case 'h':
+			spec->length = 'h';
+			break;
+		case '#':
+			spec->sharp_flag = 1;
+			break;
+		case '+':
+			spec->plus_flag = 1;
+			break;
+		case ' ':
+			spec->space_flag = 1;
+			break;
+		}
+		format++;
+	}
+	return (format);
 }
