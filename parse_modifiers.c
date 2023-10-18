c4f6c5376e3d96096d8c3a081244611c4f2c6b76
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-18 18:56:22 +0000

    Updated to correctly update the format string for futher processing

--- parse_modifiers.c
+++ parse_modifiers.c
@@ -1,47 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * parse_modifiers - handles string modifiers
  * @format: the string format
  * @spec: the format specifier
- * @buffer: the buffer to store the string
  *
  * Return: a pointer to the updated format
  */
-const char *parse_modifiers(const char *format, format_specifier *spec,
-							string_buffer *buffer)
+const char *parse_modifiers(const char *format, format_specifier *spec)
 {
-	const char *fmt_marks = "%idcsSrRuxXop";
-
-	spec->sharp_flag = 0;
-
-	while (*format && !_strchr(fmt_marks, *format))
+	while (*format)
 	{
+		/* search for conversion modifiers */
 		switch (*format)
 		{
-		case '1': case '2': case '3': case '4':
-		case '5': case '6': case '7': case '8': case '9':
-			handle_width(format, spec, buffer);
-			break;
-		case '.':
-			handle_precision(format, spec, buffer);
-			break;
-
-		case 'l':
-			spec->length = 'l';
-			break;
-		case 'h':
-			spec->length = 'h';
-			break;
 		case '#':
-			spec->sharp_flag = 1;
+			if (is_valid_sharp_specifier(*(format + 1)))
+				spec->sharp_flag = 1;
 			break;
 		case '+':
-			spec->plus_flag = 1;
+			if (is_valid_plus_specifier(*(format + 1)))
+				spec->plus_flag = 1;
 			break;
 		case ' ':
-			spec->space_flag = 1;
+			if (is_valid_space_specifier(*(format + 1)))
+				spec->space_flag = 1;
 			break;
+		default:
+			return (format); /* unknown flag/specifier encountered */
 		}
 		format++;
 	}
