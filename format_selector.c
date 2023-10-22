0e9b1bfcc7d518e0388ae3644138d1e8b3355265
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-10-22 15:04:50 +0000

    Added logic to handle length modifiers for numbers
    Also, I moved from using the switch case statements in favor of
    `if` statements so I could handle them much better

--- format_selector.c
+++ format_selector.c
@@ -27,9 +27,9 @@ int select_format_handler(const char specifier, format_specifier *spec,
 
 	for (i = 0; spec[i].specifier != '\0'; i++)
 	{
+		/* set the flag if it's available in the format string */
 		if (spec[i].specifier == specifier)
 		{
-			/* set the flag if it's available in the format string */
 			if (spec->plus_flag && is_valid_plus_specifier(specifier))
 				spec[i].plus_flag = 1;
 			if (spec->space_flag && is_valid_space_specifier(specifier))
@@ -44,12 +44,13 @@ int select_format_handler(const char specifier, format_specifier *spec,
 				spec[i].minus_flag = 1;
 			if (spec->precision && is_valid_width_specifier(specifier))
 				spec[i].precision = spec->precision;
+			if (spec->length && is_valid_length_specifier(specifier))
+				spec[i].length = spec->length;
 
 			/* invoke the appropriate function to handle the found specifier */
 			return (spec[i].handler(&spec[i], args, buffer));
 		}
 	}
-
 	/* the specifier is not found, handle it as needed */
 	append_char(buffer, '%');
 	append_char(buffer, specifier);
